dbf7ec131f072b8e3a9e195dd34fbf9d7223a92c
Sofya <Sonshik8@mail.ru>
2022-04-15 13:28:35 +0300

    пункт 12-13

--- ICG_lab2/ICG_lab2.cpp
+++ ICG_lab2/ICG_lab2.cpp
@@ -9,7 +9,11 @@
 
 GLuint VBO;
 float Scale = 0.0f;
-
+const float zF = 1.0f;
+const float zN = 100.0f;
+const float zR = zN - zF;
+const float fov = 90.0f;
+const float tanhalfFOV = tanf(glm::radians(fov / 2));
 
 static void RenderSceneCB()
 {
@@ -17,30 +21,77 @@ static void RenderSceneCB()
     glClear(GL_COLOR_BUFFER_BIT);
    // glColor3f(1.0f, 0.64f, 0.0f);
     glm::mat4 WorldMat = glm::mat4{ 1.0f };
+
+
+    //----------------------------------------------------------------------------------------------------
     //перемещение
-    /*glm::mat4 myMatrix;
-    myMatrix[0][0] = 1.0f; myMatrix[0][1] = 0.0f; myMatrix[0][2] = 0.0f; myMatrix[3][0] = sinf(Scale);
-    myMatrix[1][0] = 0.0f; myMatrix[1][1] = 1.0f; myMatrix[1][2] = 0.0f; myMatrix[1][3] = 0.0f;
-    myMatrix[2][0] = 0.0f; myMatrix[2][1] = 0.0f; myMatrix[2][2] = 1.0f; myMatrix[2][3] = 0.0f;
-    myMatrix[0][3] = 0.0f; myMatrix[3][1] = 0.0f; myMatrix[3][2] = 0.0f; myMatrix[3][3] = 1.0f;*/
+    glm::mat4 movingMat{
+    1.0f, 0.0f,  0.0f, 0.0f,
+    0.0f, 1.0f,  0.0f, 0.0f,
+    0.0f, 0.0f,  1.0f, 0.0f,
+    sinf(Scale), 0.0f, 0.0f, 1.0f
+    };
 
     //вращение
-    glm::mat4 myMatrix{
+   glm::mat4 rotationMat{
     cosf(Scale),  sinf(Scale), 0.0f, 0.0f,
     -sinf(Scale), cosf(Scale), 0.0f, 0.0f,
      0.0f,        0.0f,        1.0f, 0.0f,
      0.0f,        0.0f,        0.0f, 1.0f
     };
-    //Масштаб
-   /* glm::mat4 myMatrix;
-    myMatrix[0][0] = abs(sinf(Scale)); myMatrix[0][1] = 0.0f; myMatrix[0][2] = 0.0f; myMatrix[3][0] = 0.0f;
-    myMatrix[1][0] = 0.0f; myMatrix[1][1] = abs(cosf(Scale)); myMatrix[1][2] = 0.0f; myMatrix[1][3] = 0.0f;
-    myMatrix[2][0] = 0.0f; myMatrix[2][1] = 0.0f; myMatrix[2][2] = abs(sinf(Scale)); myMatrix[2][3] = 0.0f;
-    myMatrix[0][3] = 0.0f; myMatrix[3][1] = 0.0f; myMatrix[3][2] = 0.0f; myMatrix[3][3] = 1.0f;*/
 
-    glm::mat4 WorldMat2 = myMatrix * WorldMat;
-    glLoadMatrixf(static_cast<const float*>(glm::value_ptr(WorldMat2)));
+    //Масштаб
+    glm::mat4 conversMat{
+     abs(sinf(Scale)), 0.0f, 0.0f, 0.0f,
+     0.0f, abs(cosf(Scale)), 0.0f, 0.0f,
+     0.0f, 0.0f, abs(sinf(Scale)), 0.0f,
+     0.0f, 0.0f, 0.0f, 1.0f
+     };
+
+    //------------------------------------------------------------------------------------
+    glm::mat4 perspectMat{
+        1.0f / (tanhalfFOV * 1024/ 768), 0.0f,             0.0f, 0.0f,
+        0.0f,                1.0f / tanhalfFOV,      0.0f, 0.0f,
+        0.0f, 0.0f,          (-zN - zF) / zR,  2.0f * zF * zN / zR,
+        0.0f, 0.0f,          1.0f,              0.0f
+    };
+    //------------------------------------------------------------------------------------
+    glm::vec3 cameraPos = glm::vec3(0.3f, 0.3f, 0.3f);
+
+    glm::mat4 camTransfMat{
+        1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 1.0f, 0.0f,
+       -cameraPos.x, -cameraPos.y, -cameraPos.z, 1.0f,
+    };
+
+    glm::vec3 cameraUp = glm::vec3(0.1f, 0.3f, 0.0f);
+    glm::vec3 cameraTarget = glm::vec3(0.4f, 0.1f, 1.0f);
+    glm::vec3 cameraRight = glm::cross(cameraUp, cameraTarget);
+
+    glm::vec3 N = cameraTarget;
+    N = glm::normalize(N);
+    glm::vec3 U = cameraUp;
+    U = glm::normalize(U);
+    U = glm::cross(U, cameraTarget);
+    glm::vec3 V = glm::cross(N, U);
+    
+    glm::mat4 camRotationMat;
+    camRotationMat[0][0] = U.x; camRotationMat[0][1] = U.y; camRotationMat[0][2] = U.z; camRotationMat[0][3] = 0.0f;
+    camRotationMat[1][0] = V.x; camRotationMat[1][1] = V.y; camRotationMat[1][2] = V.z; camRotationMat[1][3] = 0.0f;
+    camRotationMat[2][0] = N.x; camRotationMat[2][1] = N.y; camRotationMat[2][2] = N.z; camRotationMat[2][3] = 0.0f;
+    camRotationMat[3][0] = 0.0f; camRotationMat[3][1] = 0.0f; camRotationMat[3][2] = 0.0f; camRotationMat[3][3] = 1.0f;
+
+   
+
+    //-------------------------------------------------------------------------------------
+     glm::mat4 WorldMat2 = perspectMat * camRotationMat * camTransfMat * conversMat * rotationMat * movingMat * WorldMat;
+     glLoadMatrixf(static_cast<const float*>(glm::value_ptr(WorldMat2)));
     Scale += 0.001f;
+    //--------------------------------------------------------------------------------------
+    
+
+
     // Разрешаем использование каждого атрибута вершины в конвейере 
    glEnableVertexAttribArray(0);
     // Обратно привязываем буфер, приготавливая для отрисовки
@@ -57,16 +108,16 @@ static void RenderSceneCB()
     
     glutSwapBuffers();
     glutPostRedisplay();
-  
+    //----------------------------------------------------------------------------------------
 }
 
 static void CreateVertexBuffer() {
     // создание вектора вершины 
-    glm::vec4 vertices[3];
+    glm::vec3 vertices[3];
     // создаем векторы с координатами x, y, z 
-    vertices[0] = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
-    vertices[1] = glm::vec4(-0.4f, 0.4f, 0.0f, 1.0f);
-    vertices[2] = glm::vec4(0.4f, 0.4f, 0.0f, 1.0f);
+    vertices[0] = glm::vec3(-1.0f, -1.0f, 2.0f);
+    vertices[1] = glm::vec3(0.0f, 1.0f, 2.0f);
+    vertices[2] = glm::vec3(1.0f, -1.0f, 2.0f);
 
     // Генерация объекта переменного типа (количество объектов, ссылка на массив для хранения)
     glGenBuffers(1, &VBO);
@@ -86,7 +137,7 @@ int main(int argc, char** argv)
     // задание размеров окна
     glutInitWindowSize(1024, 768);
     // задание начальной позиции
-    glutInitWindowPosition(100, 100);
+    glutInitWindowPosition(10, 10);
     // создание окна с заголовком "Tutorial 01"
     glutCreateWindow("Tutorial 01"); 
     // функция отрисовки 
@@ -114,7 +165,7 @@ int main(int argc, char** argv)
         return 1;
     }
     CreateVertexBuffer();
-
+   
 
     glutMainLoop();
 }
